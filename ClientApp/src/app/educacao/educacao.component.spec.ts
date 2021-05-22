56ff3e3c494ef136f50bd9f4c40fefad34da3315
BuildTools <unconfigured@null.spigotmc.org>
2021-05-22 11:17:59 -0300

    Mais implementações na página da educação

---
+++ ClientApp/src/app/educacao/educacao.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EducacaoComponent } from './educacao.component';
+
+describe('EducacaoComponent', () => {
+  let component: EducacaoComponent;
+  let fixture: ComponentFixture<EducacaoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EducacaoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EducacaoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
