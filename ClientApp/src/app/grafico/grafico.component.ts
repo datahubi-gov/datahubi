b3b29296582c3ed3e248e93ebb1fa2fa182fe1ce
Eduardo Miranda Soares <eduardo@heds.com.br>
2021-05-22 00:41:17 -0300

    Adicionado apexcharts

---
+++ ClientApp/src/app/grafico/grafico.component.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexLegend,
+  ApexStroke,
+  ApexXAxis,
+  ApexFill,
+  ApexTooltip
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  tooltip: ApexTooltip;
+  stroke: ApexStroke;
+  legend: ApexLegend;
+};
+
+@Component({
+  selector: 'grafico',
+  templateUrl: './grafico.component.html',
+  styleUrls: ['./grafico.component.scss']
+})
+export class GraficoComponent implements OnInit {
+
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions> = {
+    series: [],
+    chart: {
+      type: "bar"
+    }
+  };
+  constructor() { }
+
+  uniqueArrayByProperty(array, callback) {
+    return array.reduce((prev, item) => {
+      const v = callback(item);
+      if (!prev.includes(v)) prev.push(v)
+      return prev
+    }, [])
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.chartOptions = {
+        series: [
+          {
+            name: "Net Profit",
+            data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+          },
+          {
+            name: "Revenue",
+            data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+          },
+          {
+            name: "Free Cash Flow",
+            data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+          }
+        ],
+        chart: {
+          type: "bar",
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: "55%",
+            // endingShape: "rounded"
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ["transparent"]
+        },
+        xaxis: {
+          categories: [
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct"
+          ]
+        },
+        yaxis: {
+          title: {
+            text: "$ (thousands)"
+          }
+        },
+        fill: {
+          opacity: 1
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return "$ " + val + " thousands";
+            }
+          }
+        }
+      };
+    }, 600);
+
+  }
+
+}
