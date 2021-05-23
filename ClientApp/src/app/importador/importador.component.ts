6781924289d0fdbc0702ef9356fcbe8c34302c8c
BuildTools <unconfigured@null.spigotmc.org>
2021-05-22 21:17:51 -0300

    Implementação da página de importação

---
+++ ClientApp/src/app/importador/importador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-importador',
+  templateUrl: './importador.component.html',
+  styleUrls: ['./importador.component.css']
+})
+export class ImportadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
