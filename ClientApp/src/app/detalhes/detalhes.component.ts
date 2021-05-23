3e3808de8ef8bcb107eacb175699733b77218237
Eduardo Miranda Soares <eduardo@heds.com.br>
2021-05-22 21:33:44 -0300

    Lista detalhada filtrando e ordenando

--- ClientApp/src/app/detalhes/detalhes.component.ts
+++ ClientApp/src/app/detalhes/detalhes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ComunicService } from '../comunic.service';
+import { DadosService } from '../shared/sdkcore';
+import { firstBy } from 'thenby';
 
 @Component({
   selector: 'app-detalhes',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalhesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(DadosService) public api: DadosService,
+    @Inject(ComunicService) public comunic: ComunicService,
+  ) { }
+
+  public lista: any[] = [];
+  public textoDePesquisa: string = '';
+  public textoDePesquisaTemp: string = '';
+  public campoOrdenacao: string = 'nome';
+  public direcao: SortOrder = 'asc';
+
 
   ngOnInit(): void {
+    this.api.dadosGet('educacao', 'escolas').subscribe(x => {
+      this.lista = x;
+    });
   }
+
+  public aplicarFiltros() {
+    this.textoDePesquisa = this.textoDePesquisaTemp;
+    console.log('pesquisa', this.textoDePesquisa);
+  }
+
+  mudarOrdem(campo) {
+    if (campo != this.campoOrdenacao) {
+      this.direcao = 'asc';
+      this.campoOrdenacao = campo;
+    } else {
+      this.direcao = (this.direcao == 'asc') ? 'desc' : 'asc';
+    }
+    console.log('mudarOrderm', this.campoOrdenacao, this.direcao);
+
+  }
+
+  public get listaFiltrada(): any[] {
+    let l = this.lista;
+    if (this.textoDePesquisa) {
+      l = l.filter(item => item.nome.toLocaleLowerCase().indexOf(this.textoDePesquisa.toLocaleLowerCase()) != -1);
+    }
+    return l.sort(firstBy(this.campoOrdenacao, this.direcao));
+  }
+
+
 
 }
